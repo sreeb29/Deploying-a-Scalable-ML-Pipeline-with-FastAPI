6824daa730c85e98980512077b24e15895e7612b
sreeb29 <sree.bhattar@gmail.com>
2026-06-27 04:32:43 +0000

    updated test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -9,6 +9,22 @@ from ml.model import compute_model_metrics, train_model
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
+def test_dataset_shape():
+    # Example test for dataset sizes and types (Idea 4)
+    
+    try:
+        df = pd.read_csv('data/cleaned_census_data.csv')
+        print("Dataset file found.")
+    except FileNotFoundError:
+        pytest.fail("Dataset file not found. Please verify the path.")
+    
+    # Check if the DataFrame has rows
+    assert df.shape[0] > 0, "Dataset should have more than 0 rows."
+    # Check if it is a pandas DataFrame
+    assert isinstance(df, pd.DataFrame), "Data should be loaded as a pandas DataFrame."
+
+    #Test column type: Target column must be integer
+    #assert np.issubdtype(df['salary'].dtype, np.float), "Target column must be an integer type."
 
 
 # TODO: implement the second test. Change the function name and input as needed
